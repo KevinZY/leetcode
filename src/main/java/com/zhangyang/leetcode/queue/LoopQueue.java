63b0329c4a882576518f070cd09ed07d64de4102
zhangyang <zhangyang@derbysoft.com>
2018-10-24 19:31:43 +0800

    feat(Queue): Implementation of LoopQueue
    Implementation of LoopQueue

---
+++ src/main/java/com/zhangyang/leetcode/queue/LoopQueue.java
@@ -0,0 +1,42 @@
+package com.zhangyang.leetcode.queue;
+
+/**
+ * Created by zhangyang on 2018/10/24.
+ * LoopQueue base on Array
+ */
+public class LoopQueue {
+    private int head = 0;
+    private int tail = 0;
+    private int[] array;
+    private static final int ARRAY_SIZE = 5;
+
+    public LoopQueue() {
+        array = new int[ARRAY_SIZE];
+    }
+
+    public boolean offer(int data) {
+        if (modIncreament(tail) == head)
+            return false;
+        array[tail] = data;
+        tail = modIncreament(tail);
+        return true;
+    }
+
+    public Integer poll() {
+        if (head == tail)
+            return null;
+        int result = array[head];
+        head = modIncreament(head);
+        return result;
+    }
+
+    public Integer peek() {
+        if (head == tail)
+            return null;
+        return array[head];
+    }
+
+    private int modIncreament(int pointer) {
+        return (pointer + 1) % ARRAY_SIZE;
+    }
+}
---
+++ src/test/java/com/zhangyang/leetcode/queue/LoopQueueTest.java
@@ -0,0 +1,33 @@
+package com.zhangyang.leetcode.queue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by zhangyang on 2018/10/24.
+ */
+public class LoopQueueTest {
+    private LoopQueue loopQueue = new LoopQueue();
+
+    @Test
+    public void testLoopQueue() {
+        Assert.assertTrue(loopQueue.offer(1));
+        Assert.assertTrue(loopQueue.offer(2));
+        Assert.assertTrue(loopQueue.offer(3));
+        Assert.assertTrue(loopQueue.offer(4));
+        Assert.assertFalse(loopQueue.offer(5));
+
+        Assert.assertEquals(Integer.valueOf(1), loopQueue.peek());
+        Assert.assertEquals(Integer.valueOf(1), loopQueue.poll());
+        Assert.assertEquals(Integer.valueOf(2), loopQueue.peek());
+        Assert.assertEquals(Integer.valueOf(2), loopQueue.poll());
+        Assert.assertEquals(Integer.valueOf(3), loopQueue.peek());
+        Assert.assertEquals(Integer.valueOf(3), loopQueue.poll());
+        Assert.assertEquals(Integer.valueOf(4), loopQueue.peek());
+        Assert.assertEquals(Integer.valueOf(4), loopQueue.poll());
+        Assert.assertNull(loopQueue.peek());
+        Assert.assertNull(loopQueue.poll());
+
+
+    }
+}
